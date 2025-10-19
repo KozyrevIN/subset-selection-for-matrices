d2d1ea033f29017317754d6dbc2f00acf42c2632
tea_shroom <ivantey.kozyrev@gmail.com>
2025-10-19 17:49:46 +0000

    removed folder old from becnch. All features are alredy succesfully transfered

--- bench/old/subset_selection.h
+++
@@ -1,9 +0,0 @@
-#ifndef SUBSET_SELECTION_H
-#define SUBSET_SELECTION_H
-
-#include "matrix_generator.h"
-#include "matrix_utilities.h"
-#include "subset_selection_algorithms/all_algorithms.h"
-#include "tester.h"
-
-#endif
